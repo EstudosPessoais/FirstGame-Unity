91acb838c957e866f661d21fc054af717072f55c
bielb1b2 <gabrielvt.souza@hotmail.com>
2024-04-09 11:02:25 -0300

    first commit unity project

---
+++ Assets/Scripts/PlayerAnim.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour
+{
+
+    private Player player;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Player>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OnMove();
+        OnRun();
+    }
+
+    #region Movement
+    void OnMove()
+    {
+        if (player.direction.sqrMagnitude > 0)
+        {
+            animator.SetInteger("Transition", 1);
+        }
+        else
+        {
+            animator.SetInteger("Transition", 0);
+        }
+
+        if (player.direction.x > 0)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        if (player.direction.x < 0)
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+    }
+
+    void OnRun()
+    {
+        if(player.isRunning) 
+        {
+            animator.SetInteger("Transition", 2);
+        }
+    }
+
+    #endregion
+}
